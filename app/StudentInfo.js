34ca8343596b20a3aa9012ef62efb79654a55232
Basu30 <117108556+Basu30@users.noreply.github.com>
2023-09-12 22:18:25 -0600

    Completed

---
+++ app/StudentInfo.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function StudentInfo()
+ {
+  return (
+    <div>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <p>Name: Batsuren Damdinsuren</p>
+      <p>Course Section: CPRG 306 F</p>
+      <Link href="https://github.com/Basu30">https://github.com/Basu30</Link>
+    </div>
+  )
+}
---
+++ app/week2/week2.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Week2(){
+    return ( 
+    <div>
+        <heading><h1>My Shopping List</h1></heading>
+        <p>Name: Batsuren Damdinsuren</p>
+        <p>Course Section: CPRG 306 F</p>
+        <Link href="https://github.com/Basu30">https://github.com/Basu30</Link>
+    </div>
+    )
+}
