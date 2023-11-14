d74121ec3878ed9e2554a6e2a2a088ce1806124d
Basu30 <117108556+Basu30@users.noreply.github.com>
2023-11-14 12:36:06 -0700

    changes

--- app/week7/meal-ideas.js
+++ app/week7/meal-ideas.js
@@ -8,11 +8,12 @@ async function fetchMealIdeas({ingredient}){
     try { 
     const response = await fetch (`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);   
     const data = await response.json();
-    return data 
+    return data
     } catch (error) {
         console.error(error);
     }
-    return [];
+    return[];
+
 }
 
 
@@ -22,7 +23,7 @@ export default function MealIdeas({ingredient}){
     async function loadMealIdeas(){   
         try{ 
            const mealsIdea = await fetchMealIdeas({ingredient});
-           setMeals([mealsIdea.meals]);
+           setMeals([...mealsIdea.meals]);
         }
         catch (error){
            console.error("Error ", error);
@@ -38,9 +39,9 @@ export default function MealIdeas({ingredient}){
             <h2 className="border-black">Meal Ideas for {ingredient}</h2>          
             <ul>
                 {meals.map((meal) => ( 
-                <li key={meal.id}>
-                    <p>{meal.strMeal}</p>
-                    <img src={meal.strMealThumb} alt={meal.strMeal} />                   
+                <li key={meal.idMeal}>
+                    <p>{meal ? meal.strMeal : "error"}</p>
+                    <img src={meal ? meal.strMealThumb : "error"} />                   
                 </li> ))}
             </ul>
         </div>
