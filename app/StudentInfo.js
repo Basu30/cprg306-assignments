744141f5d17d4e77238c078ec0b067bb5776313c
Basu30 <117108556+Basu30@users.noreply.github.com>
2024-04-23 13:09:12 -0600

    changes

--- app/StudentInfo.js
+++ app/StudentInfo.js
@@ -4,7 +4,7 @@ export default function StudentInfo()
  {
   return (
     <div>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <h1>CPRG 306: Web Development 2 - Assignments.</h1>
       <p>Name: Batsuren Damdinsuren</p>
       <p>Course Section: CPRG 306 F</p>
       <Link href="https://github.com/Basu30/cprg306-assignments">https://github.com/Basu30</Link>
